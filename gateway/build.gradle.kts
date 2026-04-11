9d6f689cbeacf45d7b908d40f4f0bcd3e028d681
nikichxp <3433520+NikichXP@users.noreply.github.com>
2026-04-12 00:44:37 +0200

    updated springdoc to support actual webflux version

--- gateway/build.gradle.kts
+++ gateway/build.gradle.kts
@@ -33,7 +33,7 @@ dependencies {
 	implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
-	implementation("org.springdoc:springdoc-openapi-starter-webflux-ui:2.5.0")
+	implementation("org.springdoc:springdoc-openapi-starter-webflux-ui:2.7.0")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	
 	testImplementation("org.springframework.boot:spring-boot-starter-test") {
