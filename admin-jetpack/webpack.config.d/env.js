2f021043bf46ce20873848baff171036757fd765
nikichxp <3433520+NikichXP@users.noreply.github.com>
2026-04-19 03:23:54 +0200

    github build, cleanup

--- admin-jetpack/webpack.config.d/env.js
+++
@@ -1,7 +0,0 @@
-const webpack = require("webpack");
-
-config.plugins.push(
-    new webpack.DefinePlugin({
-        GATEWAY_BASE_URL: JSON.stringify(process.env.GATEWAY_BASE_URL || "http://localhost:8080")
-    })
-);
