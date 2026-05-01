ac2f739c6e7a091de4987f4936da6ef92fa867cd
nikichxp <3433520+NikichXP@users.noreply.github.com>
2026-05-01 23:11:22 +0200

    stripped dependencies from gateway-api

--- gateway-api/build.gradle.kts
+++ gateway-api/build.gradle.kts
@@ -1,22 +1,12 @@
 import org.gradle.jvm.tasks.Jar
-import org.springframework.boot.gradle.tasks.bundling.BootJar
-
-// Disable bootJar task to avoid Gradle 9.x compatibility issues
-tasks.named<BootJar>("bootJar") {
-	enabled = false
-}
 
 plugins {
     kotlin("jvm")
-    kotlin("plugin.spring")
-    id("org.springframework.boot")
-    id("io.spring.dependency-management")
 }
 
 group = "com.procar"
 version = "0.0.1-SNAPSHOT"
 
-// Enable regular jar task
 tasks.named<Jar>("jar") {
 	enabled = true
 	archiveClassifier.set("")
@@ -27,9 +17,5 @@ repositories {
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-webflux")
-    implementation("org.springframework.boot:spring-boot-starter-validation")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-    implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("io.swagger.core.v3:swagger-annotations:2.2.25")
 }
